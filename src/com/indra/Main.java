1fc51569f4350e74f543f574953978c1d4898756
Indra <indrabhushansingh.22@gmail.com>
2022-09-18 13:04:43 +0530

    First Checkin

---
+++ src/com/indra/Main.java
@@ -0,0 +1,12 @@
+package com.indra;
+
+import com.indra.model.Product;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+        Product product = new Product("Keyboard");
+        System.out.println(product.getName());
+    }
+}
---
+++ src/com/indra/model/Product.java
@@ -0,0 +1,16 @@
+package com.indra.model;
+
+import java.io.Serializable;
+
+public class Product {
+    private String name;
+
+    public Product(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
